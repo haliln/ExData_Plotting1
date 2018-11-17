3a8694513c33b0b7a35b3d2bb530b43621368998
Khalil Naseraldin <42477195+haliln@users.noreply.github.com>
2018-11-17 17:56:23 +0200

    Update Plot3.R
    added comments

--- Plot3.R
+++ Plot3.R
@@ -11,9 +11,11 @@ subMetering1 <- as.numeric(subSetData$Sub_metering_1)
 subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
+## create the plot file and add the required graphs to the file.
 png("plot3.png", width=480, height=480)
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+## close the connection
 dev.off()
